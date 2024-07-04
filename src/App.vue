666cb4f91979cf18a7011ae9c927a94a1fb1048f
CamelIsTheBestConvention <ansdj3523@gmail.com>
2024-07-04 22:31:14 +0900

    start vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- uri에 따라 해당 컴포넌트가 router-view를 대신함 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="category">
+      <div class="category-menu">About me</div>
+      <div class="category-menu">Skill</div>
+      <div class="category-menu">Project</div>
+      <div class="category-menu">Archiving</div>
+    </div>
+    <!-- <router-link to="/about">Go to About</router-link> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style>
+.category {
+  width: 100vw;
+  color: white;
+  background-color: navy;
+  position: fixed;
+  display: flex;
+}
+
+.category-menu {
+  font-size: 2rem;
+}
+</style>
